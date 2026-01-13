95ff628f73e624de1a4ecb7ff902007d84657982
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-01-13 22:20:01 +0000

    UPDATE: Updated workflow for new npm tokens and fixed vulnerabilities

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 /**
  *  mocha-config-generator - ☕ Mocha CLI tool to generate a configuration file boilerplate that supports different output formats.
- *  @version: v1.0.6
+ *  @version: v1.0.7
  *  @link: https://github.com/tutyamxx/mocha-config-generator
  *  @license: MIT
  **/
