bdd72b45af835285a5a18beb7282ff5d5a29c32d
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-12 18:26:10 +0000

    UPDATE: Workflow update

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 /**
  *  mocha-config-generator - ☕ Mocha CLI tool to generate a configuration file boilerplate that supports different output formats.
- *  @version: v1.1.0
+ *  @version: v1.1.1
  *  @link: https://github.com/tutyamxx/mocha-config-generator
  *  @license: MIT
  **/
